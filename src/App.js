e85f8b8c55b9668dc83542fe00e6485fcd0ab9f1
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-19 19:19:43 +0530

    Makes change to enable the feature of local starge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
+  //first fetch the data from the local storage
+  const storedItems = JSON.parse(localStorage.getItem('itemsKey'));
+  //use this as default value of items in the list
+  const [items, setItems] = useState(storedItems);
+  
+  //this useEffect updates the data stored in the local storage the state of items are changed
+  useEffect(() => {
+    localStorage.setItem('itemsKey', JSON.stringify(items));
+  }, [items]);
+
+  //a function which adds the new item typed in the input area
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  //a function which is envoked on clicking an item and it deletes the item that was clicked by use of filter function
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+        <ul>
+          {/* map through every element in the items array */}
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
