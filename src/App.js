6279e1e733821c3258f7df2620f27b9e662c83e4
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-04-20 01:10:21 +0530

    Changes the title of the list

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1>Distraction List</h1>
       </div>
       <InputArea onAdd={addItem} />
       <div>
